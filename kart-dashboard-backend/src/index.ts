12453c0091ef78d2898fef1ccd3d8084de1e1018
JuanB-png <114988701+JuanB-png@users.noreply.github.com>
2023-12-01 11:57:42 +0100

    Fixed cross origin issues

--- kart-dashboard-backend/src/index.ts
+++ kart-dashboard-backend/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import Database from './database/database.js';
 import Logger from "./database/logger.js";
+import cors from "cors";
 const log = new Logger('index');
+
+// Allow cross origin, because it's an api
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
 
 // Create a express object
 const app = express();
@@ -18,17 +27,18 @@ app.listen(process.env.HOST_PORT, () => {
 });
 
 // Create a root
-app.get("/", (req, res) => {
-    res.json("Hello this is backend!");
+app.get("/", cors(corsOptions), (req, res) => {
+    res.json("Hello, this is the backend!");
 });
 
 // Url params ALL for all data
-app.get("/ALL", async (req, res) => {
+app.get("/ALL", cors(corsOptions), async (req, res) => {
     try {
         const data = await db.getAllData();
         res.json(data);
     } catch (error) {
         console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
