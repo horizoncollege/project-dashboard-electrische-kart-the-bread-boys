abd656bc644457a314978c6959ca06b6d710a55e
Lolpower-05 <169013@student.horizoncollege.nl>
2023-12-15 15:13:44 +0100

    made chart

--- kart-dashboard-client/src/components/BarChart.js
+++ kart-dashboard-client/src/components/BarChart.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import { bar } from 'react-chartjs-2'
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-function BarChart() {
-  return (
-    <bar data={} options={}/>
-  )
+function BarChart({ chartData }) {
+  return <Bar data={chartData} />;
 }
 
-export default BarChart
+export default BarChart;
