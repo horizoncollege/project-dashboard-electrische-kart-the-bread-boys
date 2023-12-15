2b0f3676235a8a0f54a2459c2da0dc919b34a7bb
JuanB-png <114988701+JuanB-png@users.noreply.github.com>
2023-12-15 11:42:19 +0100

    Added comments

--- kart-dashboard-backend-rewrite/src/components/logger.rs
+++ kart-dashboard-backend-rewrite/src/components/logger.rs
@@ -6,17 +6,20 @@ pub struct Logger {
 
 impl Logger {
     #[allow(dead_code)]
+    // Constructor
     pub fn new(structname: &str) -> Self {
         let structname = structname.to_string();
         Logger { structname }
     }
 
+    // Log with time
     fn log_with_time(&self) -> String {
         let current_time: DateTime<Local> = Local::now();
         let formatted_time = current_time.format("%d-%m-%Y %H:%M:%S").to_string();
         formatted_time
     }
 
+    // Print out the message, level, and structname
     fn log(&self, level: &str, message: &str) {
         let level: String = level.to_string();
         let message: String = message.to_string();
@@ -25,18 +28,22 @@ impl Logger {
     }
 
     #[allow(dead_code)]
+    // Warnings
     pub fn warning(&self, message: &str) {
         self.log("WARNING", message);
     }
 
+    // Errors
     pub fn error(&self, message: &str) {
         self.log("ERROR", message);
     }
 
+    // Info
     pub fn info(&self, message: &str) {
         self.log("INFO", message);
     }
 
+    // Debug
     pub fn debug(&self, message: &str) {
         self.log("DEBUG", message);
     }
