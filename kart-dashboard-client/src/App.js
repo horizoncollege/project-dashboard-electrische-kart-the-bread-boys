7c61ae9ac90814ffc60f60fc0abab02de15038f8
timo weel <weel.timo@gmail.com>
2024-01-09 14:15:44 +0100

    small fixes

--- kart-dashboard-client/src/App.js
+++ kart-dashboard-client/src/App.js
@@ -208,28 +208,6 @@ function App() {
     options: {
       responsive: true,
       maintainAspectRatio: false,
-      scales: {
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          ticks: { color: "rgba(0, 194, 255, 1)", beginAtZero: true }
-        },
-        right: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          ticks: { color: "rgba(255, 184, 0, 1)", beginAtZero: true }
-        },
-        x: {
-          ticks: { color: 'white', beginAtZero: true }
-        },
-      },
-    },
-  };
-
-  const gyro = {
-    options: {
       plugins: {  // 'legend' now within object 'plugins {}'
         legend: {
           labels: {
