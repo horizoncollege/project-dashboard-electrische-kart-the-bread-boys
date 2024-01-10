4ef7fdc8e7fabcefd17aabca00eb082b5ee4249a
JuanB-png <114988701+JuanB-png@users.noreply.github.com>
2024-01-10 15:39:09 +0100

    Implemented rust backend

--- kart-dashboard-backend-rewrite/src/main.rs
+++ kart-dashboard-backend-rewrite/src/main.rs
@@ -1,29 +1,18 @@
 mod components;
-use components::{database::DatabaseConnection, logger::Logger, api::ApiEndpoints};
+use components::{logger::Logger, api::ApiEndpoints};
 use dotenv::dotenv;
+use actix_rt;
 
-#[tokio::main]
+#[actix_rt::main]
 async fn main() {
     dotenv().ok();
     let log: Logger = Logger::new("main");
 
     log.info("Starting API");
-    let api = ApiEndpoints::new();
+    let api = ApiEndpoints::new().await;
 
-    let mut db = match DatabaseConnection::new().await {
-        Ok(connection) => connection,
-        Err(err) => {
-            eprintln!("Failed to create DatabaseConnection: {}", err);
-            return;
-        }
-    };
-    // println!("{:?}", db.voltage().await);
-    // println!("{:?}", db.gyro().await);
-
-    if let Ok(voltage_data_array) = db.voltage().await {
-        for voltage_data in voltage_data_array {
-            println!("{}V", voltage_data.voltage);
-        }
+    match api.start_listening().await {
+        Ok(()) => println!("ctrl-c has been pressed 😂"),
+        Err(err) => eprintln!("Error occured: {}", err),
     }
-    let _ = api.start_listening().await;
 }
